85691ce2978297c601a119289a4f90c5043bc02f
QRJ <renjie.qin@brain.im>
2024-09-29 10:34:42 +0800

    fix: layer size

--- 2024-09-13-fork-imagica/ideaflow/src/hooks/useLayerPosition.ts
+++ 2024-09-13-fork-imagica/ideaflow/src/hooks/useLayerPosition.ts
@@ -31,7 +31,8 @@ const useLayerPosition = (initialState: LayerState) => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const updateMaxSize = () => {
-        setMaxSize({ width: window.innerWidth, height: window.innerHeight });
+        // 可能存在偏移，使用100%
+        setMaxSize({ width: '100%', height: '100%' });
         if (state.isMaximized) {
           setSize({ width: window.innerWidth, height: window.innerHeight });
           setPosition({ x: 0, y: 0 });
