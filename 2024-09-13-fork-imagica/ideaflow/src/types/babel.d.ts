edf9c4ad01405807bb1ecabcd02b03665a869a31
QRJ <renjie.qin@brain.im>
2024-10-08 13:12:50 +0800

    fix: dependencies

--- 2024-09-13-fork-imagica/ideaflow/src/types/babel.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@babel/preset-typescript" {
-  const preset: Record<string, unknown>;
-  export default preset;
-}
-declare module "@babel/preset-react" {
-  const preset: Record<string, unknown>;
-  export default preset;
-}
