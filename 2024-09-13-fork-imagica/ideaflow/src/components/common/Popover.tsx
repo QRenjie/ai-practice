c72d49afb4e720daed29a9b9f250b38b0dcf7e25
QRJ <renjie.qin@brain.im>
2024-10-12 13:35:53 +0800

    feat: add popover esc

--- 2024-09-13-fork-imagica/ideaflow/src/components/common/Popover.tsx
+++ 2024-09-13-fork-imagica/ideaflow/src/components/common/Popover.tsx
@@ -1,5 +1,5 @@
 import { Popover as AntdPopover, PopoverProps as AntdPopoverProps } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Popover.module.css";
 import { identity } from "lodash-es";
 import clsx from "clsx";
@@ -19,10 +19,32 @@ function Popover({
   noPadding,
   ...rest
 }: PopoverProps) {
+  // 监听 esc
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        (rest.onVisibleChange || rest.onOpenChange)?.(false);
+      }
+    };
+
+    if (rest.open) {
+      document.addEventListener("keydown", handleEsc);
+    } else {
+      document.removeEventListener("keydown", handleEsc);
+    }
+
+    return () => document.removeEventListener("keydown", handleEsc);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [rest.open]);
+
   return (
     <AntdPopover
       {...rest}
-      overlayClassName={clsx(styles.customPopover, rest.overlayClassName, noPadding && styles.noPadding)}
+      overlayClassName={clsx(
+        styles.customPopover,
+        rest.overlayClassName,
+        noPadding && styles.noPadding
+      )}
       arrow={arrow}
       getPopupContainer={relative ? identity : rest.getPopupContainer}
       content={content}
