22e5a9ccf66fb138d0b67d4cbff0944df6e51820
QRenjie <renjie.qin@brain.im>
2024-10-17 23:43:39 +0800

    fix: encrypt

--- src/utils/server/WorkspaceEncrypt.ts
+++ src/utils/server/WorkspaceEncrypt.ts
@@ -3,11 +3,12 @@ import { Buffer } from "buffer";
 
 // 配置加密算法与密钥
 const ALGORITHM = "aes-256-cbc"; // 或者使用 'aes-128-cbc'
-const KEY = crypto.randomBytes(32); // 32 字节密钥用于 AES-256（AES-128 使用 16 字节密钥）
+const KEY = Buffer.from(process.env.ENCRYPTION_KEY!, "base64"); // 32 字节密钥
 const IV = crypto.randomBytes(16); // 初始向量（IV），需 16 字节
 
+type ValueType = string;
 export class WorkspaceEncrypt {
-  static encrypt(value: string): { iv: string; data: string } {
+  static encrypt(value: ValueType): { iv: string; data: string } {
     const cipher = crypto.createCipheriv(ALGORITHM, KEY, IV);
     let encrypted = cipher.update(value, "utf8", "base64");
     encrypted += cipher.final("base64");
@@ -17,7 +18,7 @@ export class WorkspaceEncrypt {
     };
   }
 
-  static decrypt(encryptedData: { iv: string; data: string }): string {
+  static decrypt(encryptedData: { iv: string; data: string }): ValueType {
     const decipher = crypto.createDecipheriv(
       ALGORITHM,
       KEY,
